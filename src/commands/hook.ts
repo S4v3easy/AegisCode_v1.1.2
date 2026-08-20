82d6e367aec373851dd3e48d458ebc523c9d7e8d
saverio.tifi-student <saveriotifi174@gmail.com>
2026-08-20 20:06:59 +0100

    feat: test git hook automatico e fix AI suggestions

---
+++ src/commands/hook.ts
@@ -0,0 +1,62 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import process from 'node:process';
+import { Command, Args } from "@oclif/core";
+
+export default class Hook extends Command {
+    //Decleare positional argument
+    static args = {
+        action: Args.string({
+            description: 'turn on (on) and turn off (off) AegisCode Git Hook',
+            required: true,
+        }),
+    }
+
+    public async run(): Promise<void> {
+        const { args } = await this.parse(Hook);
+        const userAction = args.action;
+
+        //Check the option inside userAction and its validation
+        if (!['on', 'off'].includes(userAction)) {
+            this.error('Invalid action. Use on or off.');
+        }
+
+        //We inizialyze the path of hooks git service
+        const hooksDir = path.join(process.cwd(), '.git', 'hooks');
+        const hookPath = path.join(hooksDir, 'pre-commit');
+
+        //We have to create the pre-commit files with the bash script execute inside
+
+        //The bash script
+        const bashScript = `#!/bin/sh
+    echo "🛡️ AegisCode is analyzing your commit..."
+    npx aegis scan
+    
+    if [ $? -ne 0 ]; then
+      echo "❌ Commit REJECTED by AegisCode. Fix the violations and try again."
+      exit 1
+    fi`;
+
+        try {
+            //We create the switch logic to turn on and off the aegis automatic control
+            if(userAction === 'on') {
+                //We create the file
+                await fs.writeFile(hookPath, bashScript);
+
+                //We change the file permission
+                await fs.chmod(hookPath, '755');
+
+                this.log('AegisCode Hook ENABLED 🟢');
+            } else {
+                //We delete the file, strong process
+                await fs.rm(hookPath, { force: true });
+
+                this.log('AegisCode Hook DISABLED 🔴');
+            }
+
+        } catch(err: unknown) {
+            this.error(err instanceof Error ? err.message : 'Unknown error');
+        }
+    }
+}
+
